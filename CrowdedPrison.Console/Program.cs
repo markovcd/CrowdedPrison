ea55d34c5b21e27591c6ae87fad6141a1f6e11e7
Arkadiusz Markowski <arkadiusz.markowski@avid.com>
2020-05-31 14:03:28 +0200

    fhghgf

---
+++ CrowdedPrison.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CrowdedPrison.Console
+{
+  using System;
+
+  internal static class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("");
+    }
+  }
+}
