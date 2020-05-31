57dac7a3d5dc26980f68d189dc4620664bc2a7a1
Arkadiusz Markowski <arkadiusz.markowski@avid.com>
2020-05-31 18:43:05 +0200

    sfgsg

--- CrowdedPrison.Messenger/Entities/MessengerMessage.cs
+++ CrowdedPrison.Messenger/Entities/MessengerMessage.cs
@@ -5,10 +5,22 @@ namespace CrowdedPrison.Messenger.Entities
   public class MessengerMessage
   {
     public string Text { get; }
+    public string ThreadId { get; }
+    public string Author { get; }
+    public string Timestamp { get; }
+    public string Id { get; }
+    public bool IsRead { get; }
+    public bool IsUnsent { get; }
 
     internal MessengerMessage(FB_Message message)
     {
       Text = message.text;
+      ThreadId = message.thread_id;
+      Author = message.author;
+      Timestamp = message.timestamp;
+      Id = message.uid;
+      IsRead = message.is_read;
+      IsUnsent = message.unsent;
     }
   }
 }
--- CrowdedPrison.Wpf/ViewModels/MainViewModel.cs
+++ CrowdedPrison.Wpf/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CrowdedPrison.Messenger;
+using CrowdedPrison.Messenger.Events;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,49 @@ namespace CrowdedPrison.Wpf.ViewModels
     public MainViewModel(IMessenger messenger)
     {
       this.messenger = messenger;
+
+      messenger.ConnectionStateChanged += Messenger_ConnectionStateChanged;
+      messenger.MessageReceived += Messenger_MessageReceived;
+      messenger.MessagesDelivered += Messenger_MessagesDelivered;
+      messenger.TwoFactorRequested += Messenger_TwoFactorRequested;
+      messenger.UserLoginRequested += Messenger_UserLoginRequested;
+      messenger.MessageUnsent += Messenger_MessageUnsent;
+      messenger.Typing += Messenger_Typing;
+    }
+
+    private void Messenger_Typing(object sender, TypingEventArgs e)
+    {
+      
+    }
+
+    private void Messenger_MessageUnsent(object sender, MessageUnsentEventArgs e)
+    {
+      
+    }
+
+    private void Messenger_UserLoginRequested(object sender, UserLoginEventArgs e)
+    {
+      
+    }
+
+    private void Messenger_TwoFactorRequested(object sender, TwoFactorEventArgs e)
+    {
+      
+    }
+
+    private void Messenger_MessagesDelivered(object sender, MessagesDeliveredEventArgs e)
+    {
+      
+    }
+
+    private void Messenger_MessageReceived(object sender, MessageReceivedEventArgs e)
+    {
+
+    }
+
+    private void Messenger_ConnectionStateChanged(object sender, ConnectionStateEventArgs e)
+    {
+      
     }
   }
 }
